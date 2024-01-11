98e9bb5631f1465ed4825ee01fd9ae808d5053d8
ckakayetca <113856594+ckakayetca@users.noreply.github.com>
2024-01-11 13:51:57 +0200

    init router & store

--- src/src/main.js
+++ src/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './routes/router';
+import { createPinia } from 'pinia';
+import Loader from './components/shared/Loader.vue';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.component('Loader', Loader);
+app.use(pinia);
+app.use(router);
+app.mount('#app');
---
+++ src/src/routes/router.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/auth/Login.vue';
+import Register from '../views/auth/Register.vue';
+import MyReports from '../views/reports/MyReports.vue';
+import NewReport from '../views/reports/NewReport.vue';
+import Reports from '../views/reports/Reports.vue';
+import Error from '../views/Error.vue';
+import Home from '../views/Home.vue';
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/reports', component: Reports },
+    { path: '/reports/create', component: NewReport },
+    { path: '/auth/login', component: Login },
+    { path: '/auth/register', component: Register },
+    { path: '/my-reports', component: Reports },
+    { path: '/error', name: 'error', component: Error, props: true },
+    { path: '/:pathMatch(.*)*', component: NotFound }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
