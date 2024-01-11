307e720d37d163708a4ae02bab291a20ded4df53
ckakayetca <113856594+ckakayetca@users.noreply.github.com>
2024-01-11 13:51:47 +0200

    init components

---
+++ src/src/views/auth/Register.vue
@@ -0,0 +1,36 @@
+<script>
+import FormInput from '../../components/shared/FormInput.vue';
+import useVuelidate from '@vuelidate/core';
+import * as vuelidators from '@vuelidate/validators';
+
+export default {
+    components: { FormInput },
+    setup() {
+        return {
+            v$: useVuelidate(),
+        }
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        async onSubmit() {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <form action="" class="register" @submit.prevent="onSubmit">
+        <h1>Регистрация</h1>
+
+        <fieldset>
+
+        </fieldset>
+    </form>
+</template>
+
+<style></style>
