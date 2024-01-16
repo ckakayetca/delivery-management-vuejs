40821978c69cb658e20f4f97c3be0d7bda23e321
ckakayetca <113856594+ckakayetca@users.noreply.github.com>
2024-01-16 13:22:12 +0200

    implement home page

--- src/src/views/Home.vue
+++ src/src/views/Home.vue
@@ -5,9 +5,27 @@ export default {
 </script>
 
 <template>
-    <div>
-        Home
-    </div>
+    <h1 class="welcome">Добре дошли!</h1>
+    <button class="login-btn">Вход</button>
+    <h3>За да направите нов отчет, натиснете <button class="here">тук</button></h3>
 </template>
 
-<style></style>
+<style>
+.here {
+    display: inline;
+    width: 4em;
+    font-size: 1em;
+    padding: 0.3em 0;
+    margin-left: 0.6em;
+}
+
+.login-btn {
+    width: 40%;
+    padding: 1em 1em;
+    font-size: 3em;
+}
+
+.welcome {
+    margin-bottom: 3em;
+}
+</style>
