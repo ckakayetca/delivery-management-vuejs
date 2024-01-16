f6c42ef0b5677ce5b0df7a6d92daa03bf18c161f
ckakayetca <113856594+ckakayetca@users.noreply.github.com>
2024-01-16 13:35:18 +0200

    import auth store

---
+++ src/src/stores/authstore.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia';
+import { emptyUser } from '../utils/utils';
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        user: emptyUser,
+        isLoggedIn: false,
+    }),
+    actions: {
+        setUser(data) {
+            this.user = data;
+
+            if(this.user) {
+                this.isLoggedIn = true;
+            } else {
+                this.isLoggedIn = false;
+            }
+        }
+    }
+})
