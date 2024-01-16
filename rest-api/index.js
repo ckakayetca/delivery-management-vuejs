b7d61626e28df4154855c5df6ce7688310c7cf11
ckakayetca <113856594+ckakayetca@users.noreply.github.com>
2024-01-16 13:18:05 +0200

    add router

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const expressCfg = require('./config/expressCfg');
 const mongooseCfg = require('./config/mongooseCfg');
-
+const router = require('./routes')
 
 const app = express();
 const PORT = 3000;
 
+
 expressCfg(app);
 mongooseCfg();
+
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
