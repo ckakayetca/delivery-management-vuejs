6575b61e6b10b8452a58fb4f260bdf18e4ee2b27
Emil Georgiev <emil.tg0045@gmail.com>
2024-07-17 01:13:22 +0300

    Added vue-toastification dependency

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './routes/router';
 import { createPinia } from 'pinia';
 import Loader from './components/shared/Loader.vue';
+import Toast, { POSITION } from 'vue-toastification';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.component('Loader', Loader);
-app.use(pinia);
+app.use(Toast, {
+    position: POSITION.TOP_CENTER
+})
 app.use(router);
+app.use(pinia);
 app.mount('#app');
