4d6ae2bb0d700adcf804f71bd140823bd6c5d360
Emil Georgiev <emil.tg0045@gmail.com>
2024-07-19 01:31:44 +0300

    Setup linters in backend

---
+++ rest-api/eslint.config.js
@@ -0,0 +1,12 @@
+const prettier = require('eslint-plugin-prettier')
+
+module.exports = [
+    {
+        plugins: {
+            prettier,
+        },
+        rules: {
+            'prettier/prettier': ['error'],
+        },
+    },
+]
