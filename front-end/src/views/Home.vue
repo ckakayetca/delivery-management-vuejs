2d2ed768086da487a4fe33f0b20fece8e6ef779b
Emil Georgiev <emil.tg0045@gmail.com>
2024-07-19 02:12:15 +0300

    Home page adjustments

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,20 +1,35 @@
 <template>
-    <template v-if="!authStore.isLoggedIn">
-        <h1 class="welcome">Добре дошли!</h1>
-        <router-link :to="{ name: 'Login' }">
-            <button class="login-btn">Вход</button>
-        </router-link>
-    </template>
+    <div class="home-page">
+        <h1 class="welcome">{{ welcomeMessage }}</h1>
+        <template v-if="authStore.isLoggedIn">
+            <router-link :to="{ name: 'MyReports' }">
+                <button class="button secondary">Моите отчети</button>
+            </router-link>
 
-    <template v-else>
-        <h1 class="welcome">Добре дошъл, {{ authStore.getUser.name.split(' ')[0] }}!</h1>
-        <button>Моите отчети</button>
-        <button>Нов отчет</button>
-    </template>
+            <router-link :to="{ name: 'NewReport' }">
+                <button class="button primary">Нов отчет</button>
+            </router-link>
+        </template>
+        <template v-else>
+            <router-link :to="{ name: 'Login' }">
+                <button class="button primary">Вход</button>
+            </router-link>
+        </template>
+    </div>
 </template>
 
 <script setup>
+    import { computed } from 'vue'
+
     import { useAuthStore } from '../stores/authStore'
 
     const authStore = useAuthStore()
+
+    const welcomeMessage = computed(() => {
+        if (authStore.isLoggedIn) {
+            return `Добре дошъл, ${authStore.getUser.name.split(' ')[0]}!`
+        } else {
+            return 'Добре дошли!'
+        }
+    })
 </script>
