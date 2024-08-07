9bd6e00e5f7369aa91846e868b6a5c96ca2df00b
Emil Georgiev <emil.tg0045@gmail.com>
2024-08-07 22:33:07 +0300

    Revert changes

--- rest-api/index.js
+++ rest-api/index.js
@@ -3,9 +3,6 @@ const express = require('express')
 const expressCfg = require('./config/expressCfg')
 const mongooseCfg = require('./config/mongooseCfg')
 const router = require('./routes')
-const https = require('https')
-const http = require('http')
-const fs = require('fs')
 
 const app = express()
 const PORT = process.env.API_PORT
@@ -13,22 +10,6 @@ const PORT = process.env.API_PORT
 expressCfg(app)
 mongooseCfg()
 
-console.log(process.env)
-
-if (process.env.NODE_ENV === 'production') {
-    https
-        .createServer(
-            {
-                key: fs.readFileSync('/etc/letsencrypt/live/kiutek.eu/privkey.pem'),
-                cert: fs.readFileSync('/etc/letsencrypt/live/kiutek.eu/fullchain.pem'),
-            },
-            app,
-        )
-        .listen(PORT)
-
-    console.log(`Server is running with HTTPS`)
-} else {
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}... without HTTPS`))
-}
-
 app.use('/api', router)
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
