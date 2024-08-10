7822f1865aedd39b35ffe8b689e353b92258adc7
ckakayetca <em_eto@abv.bg>
2024-08-10 23:53:41 +0300

    fix auth bug

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,18 +1,26 @@
 import { createApp } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
 import App from './App.vue'
 import router from './routes/router'
 import { createPinia } from 'pinia'
 import Loader from './components/shared/Loader.vue'
 import Toast, { POSITION } from 'vue-toastification'
 
-const pinia = createPinia()
-const app = createApp(App)
+async function bootstrap() {
+    const pinia = createPinia()
+    const app = createApp(App)
 
-app.component('Loader', Loader)
-app.use(Toast, {
-    position: POSITION.TOP_CENTER,
-})
-app.use(pinia)
-app.use(router)
+    app.component('Loader', Loader)
+    app.use(Toast, {
+        position: POSITION.TOP_CENTER,
+    })
 
-app.mount('#app')
+    app.use(pinia)
+    await useAuthStore().init()
+
+    app.use(router)
+
+    app.mount('#app')
+}
+
+bootstrap()
